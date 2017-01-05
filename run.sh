08e300fb6320f3ab13110e5a513ab28dd3aa0db2
Ian Wright <tech@wrighting.org>
2017-01-05 13:02:19 +0000

    Changes to test 5.2 including ACE-5661

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install -Plocal alfresco:run
+MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install -Plocal alfresco:run -Dmdep.unpack.excludes="WEB-INF/lib/spring-webscripts*.jar"
