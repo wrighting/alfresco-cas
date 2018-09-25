11da17142a2f628d84a93fd795427fdf10cf0653
Ian Wright <tech@wrighting.org>
2018-09-25 14:19:01 +0100

    Start work on Alfresco 6 - just to the point of it starting - not working

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install -Plocal alfresco:run -Dmdep.unpack.excludes="WEB-INF/lib/spring-webscripts*.jar"
+MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install -Plocal alfresco:run 
+#-Dmdep.unpack.excludes="WEB-INF/lib/spring-webscripts*.jar"
